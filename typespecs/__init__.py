2a6770218c5a09056d6d59777cb1524adad105cf
Akio Taniguchi <13254278+astropenguin@users.noreply.github.com>
2025-11-05 16:30:40 +0000

    #3 Add api module

--- typespecs/__init__.py
+++ typespecs/__init__.py
@@ -1,6 +1,15 @@
-__all__ = ["typing"]
+__all__ = [
+    "Attr",
+    "api",
+    "from_dataclass",
+    "from_typehint",
+    "is_attr",
+    "typing",
+]
 __version__ = "0.1.0"
 
 
 # dependencies
+from . import api
 from . import typing
+from .api import Attr, from_dataclass, from_typehint, is_attr
