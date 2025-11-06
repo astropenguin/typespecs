d7e0b9a7b090d47bae410be07265ab284a27e927
Akio Taniguchi <13254278+astropenguin@users.noreply.github.com>
2025-11-06 17:26:54 +0000

    #7 Add spec module

--- typespecs/__init__.py
+++ typespecs/__init__.py
@@ -1,9 +1,12 @@
 __all__ = [
     "Attrs",
+    "Spec",
     "api",
     "from_dataclass",
     "from_typehint",
     "is_attrs",
+    "is_spec",
+    "spec",
     "typing",
 ]
 __version__ = "0.2.0"
@@ -11,5 +14,7 @@ __version__ = "0.2.0"
 
 # dependencies
 from . import api
+from . import spec
 from . import typing
 from .api import Attrs, from_dataclass, from_typehint, is_attrs
+from .spec import Spec, is_spec
