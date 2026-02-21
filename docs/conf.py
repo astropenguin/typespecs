4fd15df7c03bf17559c7e16012ce389429ec3fa5
Akio Taniguchi <13254278+astropenguin@users.noreply.github.com>
2026-02-21 16:40:20 +0000

    #37 Capitalize package name

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ html_static_path = ["_static"]
 html_theme = "pydata_sphinx_theme"
 html_theme_options = {
     "github_url": "https://github.com/astropenguin/typespecs",
-    "logo": {"text": "typespecs"},
+    "logo": {"text": "Typespecs"},
     "navbar_end": [
         "version-switcher",
         "theme-switcher",
